62abdadf19aa711bb72bc4bfdbf6dcbfbe84dc38
Jonas Klause <jonas.klause@brandpfeil.de>
2021-05-03 07:21:34 +0200

    refactor: timeformat dry

--- plugins/filterTimeFormat.js
+++ plugins/filterTimeFormat.js
@@ -1,31 +1,4 @@
 import Vue from 'vue'
+import timeFormat from '../lib/timeFormat.js'
 
-Vue.filter('timeFormat', function (time, format) {
-  if (format) return altFormat(time)
-
-  let { seconds, minutes, hours } = toHoursMinutesSecondes(time)
-
-  if (seconds < 10) seconds = '0' + seconds
-  if (minutes < 10) minutes = '0' + minutes
-  if (hours < 10) hours = '0' + hours
-
-  return hours + ':' + minutes + ':' + seconds
-})
-
-function toHoursMinutesSecondes(ms) {
-  return {
-    hours: Math.floor(ms / 3600000),
-    minutes: Math.floor(ms / 60000) % 60,
-    seconds: Math.floor(ms / 1000) % 60,
-  }
-}
-
-function altFormat(time) {
-  const { seconds, minutes, hours } = toHoursMinutesSecondes(time)
-  const out = []
-  if (hours) out.push(hours + 'h')
-  if (minutes) out.push(minutes + 'm')
-  if (seconds) out.push(seconds + 's')
-
-  return out.join(' ')
-}
+Vue.filter('timeFormat', timeFormat)
