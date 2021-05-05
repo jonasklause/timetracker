1373cecedf8f219fa38a5ab16a1dda21b7625a37
Jonas Klause <jonas.klause@brandpfeil.de>
2021-05-05 14:23:33 +0200

    fix: state schema updates

--- plugins/multiTabState.client.js
+++ plugins/multiTabState.client.js
@@ -1,7 +1,11 @@
+import _ from 'lodash'
 import createMultiTabState from 'vuex-multi-tab-state'
 
 export default ({ store }) => {
+  const initState = JSON.parse(JSON.stringify(store.state))
   createMultiTabState({
     statesPaths: ['items', 'history', 'settings', 'trash'],
   })(store)
+  _.merge(initState, store.state)
+  store.replaceState(initState)
 }
